10800cc3a524726d2cc9c5177f085b5da8703002
Johan Steenhoven <1254233+sbkg0002@users.noreply.github.com>
2025-09-12 09:09:36 +0200

    test-updated-mcaf-provider

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     mcaf = {
       source  = "schubergphilis/mcaf"
-      version = ">= 0.4.2"
+      version = "0.3.99"
     }
   }
   required_version = ">= 0.13"
